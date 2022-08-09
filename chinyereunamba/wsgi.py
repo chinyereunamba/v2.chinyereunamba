c1467924449d9e9ab34dce4ecabc2fb91e98419a
Chinyere Unamba <chinyereunamba17@gmail.com>
2022-08-09 19:38:51 +0100

    Almost done

--- chinyereunamba/wsgi.py
+++ chinyereunamba/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinyereunamba.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNosie(applincation)
+application = DjangoWhiteNosie(application)
