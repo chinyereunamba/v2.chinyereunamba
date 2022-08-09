04fde9ec2cc550d8caf88ff94879838ddf3d4b0c
Chinyere Unamba <chinyereunamba17@gmail.com>
2022-08-09 19:42:04 +0100

    Almost done

--- chinyereunamba/wsgi.py
+++ chinyereunamba/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNosie
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinyereunamba.settings')
 
 application = get_wsgi_application()
-# application = DjangoWhiteNosie(application)
